a6adce858c61bc716e7f98ea852876db29ee8427
Steven Cook <steve.j.cook@gmail.com>
2015-07-09 12:35:10 +1000

    Startup HTTPoison OTP app in mix setup rather than manually in code.

--- lib/stocks.ex
+++ lib/stocks.ex
@@ -36,7 +36,6 @@ defmodule Stocks do
 
 
   def get_html do
-    HTTPoison.start
     resp = HTTPoison.get!("http://www.smh.com.au/business/markets/52-week-highs?page=-1",[], [])
     resp.body
   end
